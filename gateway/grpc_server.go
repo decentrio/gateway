f3918a0526a6b352cf3f8360af433597f3f1ad52
Master Pi <minh.vln140501@gmail.com>
2025-03-07 12:33:41 +0100

    simple http forward for /block

--- gateway/grpc_server.go
+++ gateway/grpc_server.go
@@ -2,7 +2,7 @@ package gateway
 
 import (
 	"fmt"
-	"net/http"
+	// "net/http"
 	"os"
 
 	// "google.golang.org/grpc"
@@ -12,14 +12,14 @@ import (
 func Start_GRPC_Server(server *Server) {
 	fmt.Printf("Starting server on port %d\n", server.Port)
 	// http.HandleFunc("/", server.handleRequest)
-	http.HandleFunc("/",
-		func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-		})
-	err := http.ListenAndServe(fmt.Sprintf(":%d", server.Port), nil)
-	if err != nil {
-		fmt.Printf("Error starting server: %v\n", err)
-	}
+	// http.HandleFunc("/",
+	// 	func(w http.ResponseWriter, r *http.Request) {
+	// 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	// 	})
+	// err := http.ListenAndServe(fmt.Sprintf(":%d", server.Port), nil)
+	// if err != nil {
+	// 	fmt.Printf("Error starting server: %v\n", err)
+	// }
 }
 
 func Shutdown_GRPC_Server(server *Server) {
