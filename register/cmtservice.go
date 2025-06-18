659546ef2301f11db423883036b530e8dc08c892
Đông Liều <duongduydongtq@gmail.com>
2025-06-18 16:11:05 +0700

    TLS

--- register/cmtservice.go
+++ register/cmtservice.go
@@ -2,11 +2,16 @@ package register
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
+	"strings"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+
+	// "google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/metadata"
 
 	tmservice "github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
@@ -33,8 +38,18 @@ func (s *CustomTMService) GetBlockByHeight(ctx context.Context, req *tmservice.G
 		return nil, status.Errorf(codes.InvalidArgument, "No node found for height %d", req.Height)
 	}
 
+	var err error
+	var conn *grpc.ClientConn
+	if strings.HasSuffix(node.GRPC, ":443") {
+		tlsConfig := &tls.Config{InsecureSkipVerify: true}
+		conn, err = grpc.DialContext(ctx, node.GRPC, grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
+	} else {
+		conn, err = grpc.DialContext(ctx, node.GRPC, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+
 	// Dial node gRPC
-	conn, err := grpc.Dial(node.GRPC, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// creds := credentials.NewClientTLSFromCert(nil, "")
+	// conn, err := grpc.Dial(node.GRPC, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		return nil, err
 	}
