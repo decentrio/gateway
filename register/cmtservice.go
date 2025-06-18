ce5ee349b24ca767cac66bf898f6c9032ac0d3ea
netflix-and-chilll <minh@decentrio.ventures>
2025-06-18 11:32:51 +0200

    minor

--- register/cmtservice.go
+++ register/cmtservice.go
@@ -13,9 +13,6 @@ import (
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 
-	// "google.golang.org/grpc/credentials/insecure"
-	// "google.golang.org/grpc/metadata"
-
 	tmservice "github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
 	"github.com/decentrio/gateway/config"
 	"github.com/gogo/status"
@@ -26,15 +23,6 @@ type CustomTMService struct {
 }
 
 func (s *CustomTMService) GetBlockByHeight(ctx context.Context, req *tmservice.GetBlockByHeightRequest) (*tmservice.GetBlockByHeightResponse, error) {
-	// Tạo metadata từ height
-	// md := metadata.New(map[string]string{
-	// 	"x-cosmos-block-height": fmt.Sprintf("%d", req.Height),
-	// })
-
-	// Tạo context mới có metadata
-	// ctx = metadata.NewOutgoingContext(ctx, md)
-
-	// Lấy node từ config theo height
 	node := config.GetNodebyHeight(uint64(req.Height))
 	if node == nil {
 		return nil, status.Errorf(codes.InvalidArgument, "No node found for height %d", req.Height)
@@ -51,18 +39,14 @@ func (s *CustomTMService) GetBlockByHeight(ctx context.Context, req *tmservice.G
 		conn, err = grpc.DialContext(ctx, node.GRPC, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	}
 
-	// Dial node gRPC
-	// creds := credentials.NewClientTLSFromCert(nil, "")
-	// conn, err := grpc.Dial(node.GRPC, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		return nil, err
 	}
 	defer conn.Close()
 
-	// Tạo client gốc
 	client := tmservice.NewServiceClient(conn)
-
-	// Gửi lại request tới backend
 	return client.GetBlockByHeight(ctx, req)
 
 }
+
+// todo: manually add other query types if needed
